9dba7a77f83683affce683eca765101d8912f5c0
yveschiu <yves.s.c.chiu@gmail.com>
2023-06-18 17:41:23 -0400

    feat(blade): add basic layout and task related blade templates

---
+++ resources/views/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('title', 'The list of tasks')
+
+@section('content')
+    <div>
+        {{-- @if (count($tasks))
+            <ul>
+                @foreach ($tasks as $task)
+                    <li>
+                        <a href="{{ route('tasks.show', ['id' => $task->id]) }}">
+                            {{ $task->title }}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+        @else
+            <p>no tasks</p>
+        @endif --}}
+        <ul>
+            @forelse ($tasks as $task)
+                <li>
+                    <a href="{{ route('tasks.show', ['id' => $task->id]) }}">
+                        {{ $task->title }}
+                    </a>
+                </li>
+            @empty
+                <p>no tasks</p>
+            @endforelse
+        </ul>
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel 10 Task List App</title>
+    @yield('styles')
+</head>
+
+<body>
+    <h1>
+        @yield('title')
+    </h1>
+    <div>
+        @if (session()->has('success'))
+            <div>{{ session('success') }}</div>
+        @endif
+        @yield('content')
+    </div>
+</body>
+
+</html>
