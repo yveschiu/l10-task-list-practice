c0cd7091f2de13e63d2d21b11b01899192cb5658
yveschiu <yves.s.c.chiu@gmail.com>
2023-06-18 17:42:00 -0400

    feat(route): add task related routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +17,56 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('tasks.index');
+});
+
+Route::get('/tasks', function () {
+    return view('index', [
+        'tasks' => Task::latest()->get(),
+    ]);
+})->name('tasks.index');
+
+Route::get('/tasks/create', function () {
+    return view('create');
+})->name('tasks.create');
+
+Route::get('/tasks/{task}/edit', function (Task $task) {
+    return view('edit', [
+        'task' => $task,
+    ]);
+})->name('tasks.edit');
+
+Route::get('/tasks/{id}', function ($id) {
+    // The findOrFail and firstOrFail methods will retrieve the first result of the query; however, if no result is found, an Illuminate\Database\Eloquent\ModelNotFoundException will be thrown:
+    // If the ModelNotFoundException is not caught, a 404 HTTP response is automatically sent back to the client:
+    return view('show', [
+        'task' => Task::findOrFail($id),
+    ]);
+})->name('tasks.show');
+
+Route::post('/tasks', function (Request $request) {
+    $data = $request->validate(
+        [
+            'title' => 'required|max:255',
+            'description' => 'required|max:255',
+            // 'long_description' => 'nullable',
+            'long_description' => 'required|max:255',
+            'completed' => 'nullable',
+        ]
+    );
+    $task = new Task();
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->completed = $data['completed'] ?? false;
+    $task->save();
+    return redirect()
+        ->route('tasks.show', $task->id)
+        ->with('success', 'Task created successfully.');
+})->name('tasks.store');
+
+
+
+Route::fallback(function () {
+    return 'Sorry, the page you are looking for could not be found.';
 });
